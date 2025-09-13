48d2103a724400fd9175d6fa09f1b35587289cd1
ARTMARVL <vladyslav.anishchenko@gmail.com>
2025-09-13 19:06:55 +0200

    HW 2

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -13,7 +13,7 @@ export const initMongoConnection = async () => {
         const url = getEnvVar("MONGODB_URL");
         const db = getEnvVar("MONGODB_DB");
 
-        await mongoose.connect(`mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`);
+        await mongoose.connect(`mongodb+srv://mongodb+srv://vladyslavanishchenko_db_user:E4uJf4i5QSgFJtGb@artmarvl.wko9smn.mongodb.net/?retryWrites=true&w=majority&appName=ARTMARVL`);
 
         console.log("Mongo connection successfully established!");
 
