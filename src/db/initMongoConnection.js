13856622a0b3a2cbb4606643743d9b8125010d19
ARTMARVL <vladyslav.anishchenko@gmail.com>
2025-09-13 23:07:24 +0200

    HW3

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,4 +1,3 @@
-
 import mongoose from "mongoose";
 
 import { getEnvVar } from "../utils/getEnvVar.js";
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -5,8 +5,32 @@ export const getAllContacts = async () => {
     return contacts;
 };
 
-
-export const getContactsById = async (contactsId) => {
-    const contact = await ContactsCollection.findById(contactsId);
+export const getContactsById = async (contactId) => {
+    const contact = await ContactsCollection.findById(contactId);
     return contact;
 };
+
+export function createContact(payload) {
+    return ContactsCollection.create(payload);
+}
+
+export function deleteContact(contactId) {
+    return ContactsCollection.findByIdAndDelete(contactId);
+}
+
+export function updateContact(contactId, payload) {
+    return ContactsCollection.findByIdAndUpdate(contactId, payload, { new: true });
+}
+
+export async function replaceContact(contactId, payload) {
+    const result = await ContactsCollection.findByIdAndUpdate(contactId, payload, {
+        new: true,
+        upsert: true,
+        includeResultMetadata: true,
+    });
+
+    return {
+        value: result.value,
+        updatedExisting: result.lastErrorObject.updatedExisting,
+    };
+}
